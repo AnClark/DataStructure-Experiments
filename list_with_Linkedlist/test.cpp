8a5f524602ab1396e256a246bdd511d62e94abe9
AnClark Liu <anclark@hust.edu.cn>
2017-05-08 04:46:20 +0800

    Improved testor

--- list_with_Linkedlist/test.cpp
+++ list_with_Linkedlist/test.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 LinkedListClass *ll = new LinkedListClass();
 void llState();
+#define ERROR_WARNING cout<<"*** ERROR OCCURRED ***"<<endl
 
 // For debug
 int main()
@@ -15,6 +16,7 @@ int main()
 
     // TEST FOR CREATE & INSERT
     cout<<"-------- NOW LET'S TEST INSERT --------"<<endl;
+
     while(cin>>d  &&  d != 0){
         ll->ListInsert(0, d);
     }
@@ -24,9 +26,16 @@ int main()
     // TEST FOR REMOVE
     cout<<"-------- NOW LET'S TEST REMOVE --------"<<endl;
     cout<<"\t(-1 for EOL)"<<endl;
-    while(cin>>i  &&  i != -1){
-        ll->ListDelete(i, d);
-        cout<<"--- Removed value: "<<d<<endl;
+
+    while(cin>>i  &&  i != -1)
+    {
+        int res = ll->ListDelete(i, d);
+
+        if(res == OK)
+            cout<<"--- Removed value: "<<d<<endl;
+        else if(res == ERROR)
+            ERROR_WARNING;
+
         llState();
     }
     
@@ -39,6 +48,10 @@ void llState()
     cout<<"Is empty list?\t"<<ll->ListEmpty()<<endl;
     cout<<"Length? \t"<<ll->ListLength()<<endl;
 
-    cout<<"Your linked list is:"<<endl;
-    ll->PrintList();
+    if(!ll->ListEmpty())
+    {
+        cout<<"Your linked list is:"<<endl;
+        ll->PrintList();
+    }
 }
+
